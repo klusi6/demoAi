a1d14bc2e94dac2673a0365a234e7e495ca60b66
vasek <klusacekv@icloud.com>
2024-01-05 21:25:02 +0100

    final+ some fun with console

--- src/main/java/org/example/demoai/DemoAiApplication.java
+++ src/main/java/org/example/demoai/DemoAiApplication.java
@@ -1,11 +1,12 @@
 package org.example.demoai;
 
-import org.example.demoai.model.PromptAndResponse;
-import org.example.demoai.repository.PromptAndResponseRepository;
+import org.example.demoai.service.PromptAndResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoAiApplication implements CommandLineRunner {
@@ -13,14 +14,35 @@ public class DemoAiApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(DemoAiApplication.class, args);
     }
+
+    @Autowired
+    PromptAndResponseService promptAndResponseService;
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("(-_-) Sleeping (-_-)");
+
         for (int i = 0; i <= 100; i++) {
             System.out.print("\r");
-            System.out.print("Loading: " + i + "%");
+            System.out.print("---Loading: " + i + "%---");
+            if (i == 100) {
+                System.out.println();
+                System.out.println("-------Done!-------");
+                break;
+            }
             Thread.sleep(20);
         }
-        System.err.println(("\n"+"(O_O) Awake (O_O)" + "\nWelcome"));
-    }
+        do {
+            System.out.println("Enter your prompt:");
+            Scanner scanner = new Scanner(System.in);
+            String prompt = scanner.nextLine();
+            System.out.println(promptAndResponseService.chat(prompt));
+            System.out.println("Do you want to continue? (y/any key to exit)");
+            String answer = scanner.nextLine();
+            if (!answer.equals("y")) {
+                scanner.close();
+                return;
+            }
+        } while (true);
+
+        }
 }
